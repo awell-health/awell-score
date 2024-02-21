5f04f9bdd83bc12a8a3810073295160f75da9d02
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2024-02-21 21:05:07 +0100

    feat(): v2.27

---
+++ src/calculation_suite/calculations/iief5/__testdata__/iief5_test_responses.ts
@@ -0,0 +1,34 @@
+export const best_response = {
+  IIEF5_Q01: 5,
+  IIEF5_Q02: 5,
+  IIEF5_Q03: 5,
+  IIEF5_Q04: 5,
+  IIEF5_Q05: 5,
+}
+
+export const median_response = {
+  IIEF5_Q01: 3,
+  IIEF5_Q02: 3,
+  IIEF5_Q03: 3,
+  IIEF5_Q04: 3,
+  IIEF5_Q05: 3,
+}
+
+export const worst_response = {
+  IIEF5_Q01: 1,
+  IIEF5_Q02: 0,
+  IIEF5_Q03: 0,
+  IIEF5_Q04: 0,
+  IIEF5_Q05: 0,
+}
+
+/**
+ * Expected score: 8
+ */
+export const random_response = {
+  IIEF5_Q01: 1,
+  IIEF5_Q02: 1,
+  IIEF5_Q03: 2,
+  IIEF5_Q04: 0,
+  IIEF5_Q05: 4,
+}
