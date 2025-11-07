7607df5d24aebc97fef625d85c26af4f9d53984d
Abigaila Ekiert <arekiert@gmail.com>
2025-11-07 11:35:57 +0000

    add polish translations to hads

--- src/scores/hads/definition/hads_output.ts
+++ src/scores/hads/definition/hads_output.ts
@@ -3,11 +3,11 @@ import type { ScoreOutputSchemaType } from '../../../types'
 
 export const HADS_OUTPUT = {
   FEAR: {
-    label: { en: 'Fear' },
+    label: { en: 'Fear', nl: 'Angst', pl: 'Strach' },
     type: z.number(),
   },
   DEPRESSION: {
-    label: { en: 'Depression' },
+    label: { en: 'Depression', nl: 'Depressie', pl: 'Depresja' },
     type: z.number(),
   },
 } satisfies ScoreOutputSchemaType
