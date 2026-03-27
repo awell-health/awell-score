5f9e43f2972251ff427ed79a280bff9c93b58ced
Abigaila Ekiert <arekiert@gmail.com>
2026-03-27 10:55:43 +0000

    add language support for paid_20

--- src/scores/PAID_20/definition/paid_20_interpretation.ts
+++ src/scores/PAID_20/definition/paid_20_interpretation.ts
@@ -1,4 +1,21 @@
-export const PAID20_INTERPRATION = {
-  SEVERE: 'Severe diabetes distress',
-  NOT_SEVERE: 'Not severe diabetes distress',
+export const PAID20_INTERPRETATION_LABEL = {
+  SEVERE: {
+    en: 'Severe diabetes distress',
+    pl: 'Ciężki dystres cukrzycowy',
+  },
+  NOT_SEVERE: {
+    en: 'Not severe diabetes distress',
+    pl: 'Brak ciężkiego dystresu cukrzycowego',
+  },
+}
+
+export const PAID20_INTERPRETATION_CODE = {
+  SEVERE: {
+    en: 'SEVERE',
+    pl: 'CIĘŻKI',
+  },
+  NOT_SEVERE: {
+    en: 'NOT_SEVERE',
+    pl: 'BRAK_CIĘŻKIEGO',
+  },
 }
