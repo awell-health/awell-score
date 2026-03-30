af6c42642b2f527b90bc30916ff9c2a422fddd52
Abigaila Ekiert <arekiert@gmail.com>
2026-03-30 12:22:32 +0100

    update-polish-translations

--- src/scores/PAID_5/definition/paid_5_interpretation.ts
+++ src/scores/PAID_5/definition/paid_5_interpretation.ts
@@ -1,22 +1,22 @@
 export const PAID5_INTERPRETATION_LABEL = {
   POSSIBLE_DISTRESS: {
     en: 'Possible diabetes related emotional distress, which warrants further assessment',
-    pl: 'Możliwe zaburzenia emocjonalne związane z cukrzycą, wymagające dalszej oceny',
+    pl: 'Podejrzenie istotnego stresu związanego z cukrzycą',
   },
   NO_DISTRESS: {
     en: 'No indication of diabetes related emotional distress',
-    pl: 'Brak wskazań na zaburzenia emocjonalne związane z cukrzycą',
+    pl: 'Brak lub niskie nasilenie stresu związanego z cukrzycą',
   },
 }
 
 export const PAID5_INTERPRETATION_CODE = {
   POSSIBLE_DISTRESS: {
     en: 'POSSIBLE_DISTRESS',
-    pl: 'MOŻLIWY_DYSTRES',
+    pl: 'MOŻLIWY_STRES',
   },
   NO_DISTRESS: {
     en: 'NO_DISTRESS',
-    pl: 'BRAK_DYSTRESU',
+    pl: 'BRAK_STRESU',
   },
 }
 
